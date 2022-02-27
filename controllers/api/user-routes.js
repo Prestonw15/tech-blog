e4de5f537cd782d88a2d53d6483dd1f1489baf0e
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-27 13:36:56 -0700

    started working on my user routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,109 @@
+// user router
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+
+// GET /api/users
+// gets all users
+router.get('/', (req, res) => {
+    //access the user model and run .findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] } 
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'There has been an error' });
+    });
+});
+
+//GET /api/users/1
+// gets one user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+          {
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+          },
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          }
+        ]
+      
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'There has been an error' });
+    });
+
+});
+
+//POST /api/users
+// adds a user
+router.post('/', (req, res) => {
+    // expects {username: 'Jillium', email: 'holmesjillanne@gmail.com', 'password: 'password1234'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: 'There has been an error' });
+    });
+
+});
+//login route
+router.post('/login', (req, res) => {
+    // expects {username: 'username', password: 'password1234'}
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: "You are now logged in!" });
+      });
+      
+    });
+  });
