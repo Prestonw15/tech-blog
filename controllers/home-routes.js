aebae87747b63f48e37e830e0e410d48cb23148c
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-27 13:53:22 -0700

    added the login signup and logout calls in the home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,78 @@ const router = require('express').Router();
 const req = require('express/lib/request');
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/logout', (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+})
+
+// route for cookies
+router.get('/', (req, res) => {
+    console.log(req.session);
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+
+// route for session/cookies
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    // other logic...
+});
