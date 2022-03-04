be9cd659da4eebfd03d28ef263751f877aefb854
Prestonw15 <prestonwatson.15@gmail.com>
2022-03-03 21:46:23 -0700

    failed to get my dotenv to go through so I gave up steal my data info if you'd like

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,18 @@
 const Sequelize = require('sequelize');
 
 // require dotenv package to hide sensitive info
-require('dotenv').config();
+// require('dotenv').config();
+// const name = process.env.DB_NAME
+// console.log(name)
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
 // connects us to the db
-sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+// sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+sequelize = new Sequelize("tech_blog", "root", "Tayson123", {
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
 });
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers/');
 const session = require('express-session');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
-
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3006;
